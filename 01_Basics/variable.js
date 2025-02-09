21a018909d141cfb7cba55436aff10771a8630be
Tauhid Shekhar <161128044+TauhidShekhar@users.noreply.github.com>
2025-02-09 11:44:06 +0000

    variable_file_add

---
+++ 01_Basics/variable.js
@@ -0,0 +1,30 @@
+const accountId = 12345
+
+let accountEmail="tauhid@gmail.com"
+var accuntPass="2345"
+let accountState;
+
+accountCity="bettiah"
+
+
+// accountId=3456  // no allowed
+
+console.log(accountId) 
+
+/*
+Prefer not to use var 
+   becouse of issue on block scope and functional scope
+*/
+
+accountEmail="shekhar@gmail.com"
+accuntPass="3750"
+
+accountCity="amritsar"
+
+console.table([accountId,accountEmail,accuntPass,accountCity,accountState])
+
+// console.log(accountEmail);
+// console.log(accuntPass);
+// console.log(accountCity);
+
+
