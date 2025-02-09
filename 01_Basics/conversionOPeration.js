58fb8099a13afec697284c2de77f1262e1fc8ec1
Tauhid Shekhar <161128044+TauhidShekhar@users.noreply.github.com>
2025-02-09 12:54:49 +0000

    conversion of dataType

---
+++ 01_Basics/conversionOPeration.js
@@ -0,0 +1,34 @@
+let score="tauhid"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33 
+// 33adf => NaN  not any number
+// true=> 1; false =>0
+
+
+
+let isLoggedIn="tauhid"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 =>true; 0 => false
+// "" => false
+// "tauhid" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(someNumber);
+console.log(typeof someNumber);
+
+
+
+
