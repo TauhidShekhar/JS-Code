670a589923915749b64f2d0624f89074c4775351
Tauhid Shekhar <161128044+TauhidShekhar@users.noreply.github.com>
2025-02-09 12:26:49 +0000

    dataType

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict"// treat all js code as newer version
+// alert (3+3)// we aree using nodejs, not browser
+
+console.log(3+
+    5 //code readability should be high 
+);
+
+console.log("Shekhar")
+
+let name="Tauhid"
+let age=20;
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// boolean => standalone value
+// undifined =>
+// symbol => unique 
+
+// object
+
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+
+
